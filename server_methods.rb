df414a67d3bb7ff33836022db7f06c6c9ac51d9d
K <celfrost@hotmail.com>
2014-06-01 16:33:20 -0400

    Adding stylings

--- server_methods.rb
+++ server_methods.rb
@@ -13,8 +13,7 @@ def get_recipes()
   query = " SELECT DISTINCT ON (recipes.name) recipes.id AS recipe_id, recipes.name AS name, recipes.instructions AS instructions, recipes.description AS description, ingredients.name AS ingredients
             FROM recipes
             JOIN ingredients ON ingredients.recipe_id = recipes.id
-            ORDER BY name
-            LIMIT 50;"
+            ORDER BY name;"
 
   recipes = db_connection do |conn|
     conn.exec(query)
